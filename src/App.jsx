5c5f0a190e65d5cacf23ae69220701b39627862c
Rikcher <nick.richardson.4884@gmail.com>
2024-04-28 11:06:56 +0700

    Adding planet canvas

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect, useRef } from 'react';
 import './App.css'
+import { Application } from '@splinetool/runtime';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const canvasRef = useRef(null); // Create a ref to reference the canvas element
+
+  useEffect(() => {
+      if (canvasRef.current) { // Check if the canvasRef is available
+        const app = new Application(canvasRef.current); // Initialize the Spline application with the canvas element
+        app.load('https://prod.spline.design/gsBeBMWmNSKzvVr9/scene.splinecode'); // Load the Spline scene
+      }
+  }, []); // Empty dependency array means this effect runs once after the initial render
 
   return (
-    <>
-
-    </>
-  )
+      <div className="App">
+        <header className="App-header">
+          <canvas ref={canvasRef} id="canvas3d" /> {/* Use the ref here */}
+        </header>
+      </div>
+  );
 }
 
-export default App
+export default App;
