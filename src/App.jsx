5dcea165498e5fb3034d109a5ce0124f5d0ca158
Rikcher <nick.richardson.4884@gmail.com>
2024-05-08 12:16:52 +0700

    Add interaction functionality to planet and  landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,49 @@
 import { useState, useEffect, useRef } from 'react';
-import './App.css'
+import '/src/styles/css/App.css'
 import { Application } from '@splinetool/runtime';
 
 function App() {
   const canvasRef = useRef(null); // Create a ref to reference the canvas element
+  const [text, setText] = useState("not changed")
+  const [backgroundActive, setBackgroundActive] = useState(false)
+  const [sceneLoaed, setSceneLoaded] = useState(false)
 
   useEffect(() => {
+    const loadApp = async () => {
       if (canvasRef.current) { // Check if the canvasRef is available
         const app = new Application(canvasRef.current); // Initialize the Spline application with the canvas element
-        console.log(app)
-        app.load('https://prod.spline.design/gsBeBMWmNSKzvVr9/scene.splinecode')
-        .then(() => {
-          app.setVariables({test_text: "TEST"})
-        }); // Load the Spline scene
+        try {
+          await app.load('https://prod.spline.design/gsBeBMWmNSKzvVr9/scene.splinecode');
+          setSceneLoaded(true)
+          app.addEventListener('mouseDown', (e) => {
+            if (e.target.name === "1_place") {
+              setText("First Place")
+            } else if (e.target.name === "2_place") {
+              setText("Secon Place")
+            } else if (e.target.name === "3_place") {
+              setText("Third Place")
+            } else if (e.target.name === "Trigger_to_zoom_in") {
+              setBackgroundActive(true)
+            }
+          });
+        } catch (error) {
+          console.error("Failed to load the Spline scene:", error);
+        }
       }
+    };
+  
+    loadApp(); // Call the async function
   }, []); // Empty dependency array means this effect runs once after the initial render
+  
 
   return (
       <div className="App">
-          <canvas ref={canvasRef} id="canvas3d" /> {/* Use the ref here */}
+        <div className={`background ${backgroundActive ? "active" : ""}`}></div>
+        <div className="landing_page_content">
+          <h1 className={`hero_title ${backgroundActive ? "hide" : ""}`}>Leviathan<span className='apostroph'>'</span>s Keep</h1>
+          <p className={`hero_subtitle ${backgroundActive ? "hide" : ""}`}>humanity<span className='apostroph'>'</span>s last sight</p>
+        </div>
+        <canvas style={{opacity: sceneLoaed ? "1" : "0"}} ref={canvasRef} id="canvas3d" />
       </div>
   );
 }
